6eeb7dfccc11cc84e0f2aad6a410bc8d5a231ba9
Moses-Hamon <moses_hamon@bigpond.com>
2018-08-26 15:29:05 +1000

    Add project files.

---
+++ MusicApp/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MusicApp.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Track List </h1>
+
+            <a href="Edit.aspx">Create New Track</a> <br />
+
+            <asp:GridView ID="gvList" runat="server" OnSelectedIndexChanged="gvList_SelectedIndexChanged">
+                <EmptyDataTemplate>
+                    <span>No data to display.</span>
+                </EmptyDataTemplate>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ MusicApp/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MusicApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // the object that accesses the web service for us is in
+            //the namespace we specified, then <ServiceName>SoapClient
+            var svc = new MusicServiceProxy.MusicServiceSoapClient();
+            // you can then access the [WebMethod] tagged methods in you web service like this:
+            gvList.DataSource = svc.SelectAll();
+            gvList.DataBind();
+
+            // note: the types exposed through the WebService (via return types or arguments)
+            // are also translated and live within the MusicServiceProxy
+            // -> this means you have access to the Music model object here in the app
+        }
+
+        protected void gvList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var id = gvList.SelectedRow.Cells[1].Text;
+            Response.Redirect($"~/Edit.aspx?id=" + id);
+        }
+    }
+}
